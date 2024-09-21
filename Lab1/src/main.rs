9ef9b46a9371877aba67cc4857aa7e874f5ad211
xairaven <alexkovalevkpi@gmail.com>
2024-09-21 05:01:58 +0300

    Lab 1: Added logging

--- Lab1/src/main.rs
+++ Lab1/src/main.rs
@@ -1,3 +1,14 @@
+use log::LevelFilter;
+
+pub mod error;
+pub mod logger;
+
+pub const LOG_FILE_NAME: &str = "log.txt";
+pub const LOG_LEVEL: LevelFilter = LevelFilter::Info;
+
 fn main() {
-    println!("Hello, world!");
+    logger::init(LOG_LEVEL, LOG_FILE_NAME).unwrap_or_else(|err| {
+        println!("Logger initialization failed. Error: {}", err);
+        std::process::exit(1);
+    });
 }
