fb39b93ff64271325c98097112a093b3c97e3fa9
xairaven <alexkovalevkpi@gmail.com>
2024-10-12 22:11:20 +0300

    Lab 2, Feat: Additional colors & strokes for tests

--- Lab2/src/ui/styles/colors.rs
+++ Lab2/src/ui/styles/colors.rs
@@ -1,7 +1,11 @@
 use egui::Color32;
 
+pub const AQUA: Color32 = Color32::from_rgb(0, 255, 255);
 pub const BLACK: Color32 = Color32::from_rgb(0, 0, 0);
 pub const DARK_RED: Color32 = Color32::from_rgb(198, 55, 57);
 pub const GRAY: Color32 = Color32::from_rgb(200, 200, 200);
 pub const LIME: Color32 = Color32::from_rgb(130, 245, 131);
+pub const ORANGE: Color32 = Color32::from_rgb(255, 127, 39);
+pub const PURPLE: Color32 = Color32::from_rgb(163, 73, 164);
 pub const RED: Color32 = Color32::from_rgb(255, 0, 0);
+pub const YELLOW: Color32 = Color32::from_rgb(255, 242, 0);
--- Lab2/src/ui/styles/strokes.rs
+++ Lab2/src/ui/styles/strokes.rs
@@ -13,6 +13,22 @@ pub fn grid_gray() -> Stroke {
     Stroke::new(0.8, colors::GRAY)
 }
 
+pub fn model_aqua() -> Stroke {
+    Stroke::new(2.0, colors::AQUA)
+}
+
 pub fn model_black() -> Stroke {
     Stroke::new(2.0, colors::BLACK)
+}
+
+pub fn model_orange() -> Stroke {
+    Stroke::new(2.0, colors::ORANGE)
+}
+
+pub fn model_purple() -> Stroke {
+    Stroke::new(2.0, colors::PURPLE)
+}
+
+pub fn model_yellow() -> Stroke {
+    Stroke::new(2.0, colors::YELLOW)
 }
