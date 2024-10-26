d2adcde7ea06d1309742dd76bab8db3b3c8b38a3
xairaven <alexkovalevkpi@gmail.com>
2024-10-26 22:17:11 +0300

    Lab 4, Feat: Bezier point Smoothness Type

--- Lab4/src/models/bezier_point.rs
+++ Lab4/src/models/bezier_point.rs
@@ -4,21 +4,27 @@ use crate::traits::positionable::Positionable;
 #[derive(Debug, Copy, Clone)]
 pub struct BezierPoint {
     pub point: MoveablePoint,
+
     pub kind: BezierPointType,
+    pub smoothness: SmoothnessType,
 }
 
 impl BezierPoint {
     pub fn defining(x: f32, y: f32) -> Self {
         Self {
             point: MoveablePoint::new(x, y),
+
             kind: BezierPointType::Defining,
+            smoothness: SmoothnessType::None,
         }
     }
 
     pub fn control(x: f32, y: f32) -> Self {
         Self {
             point: MoveablePoint::new(x, y),
+
             kind: BezierPointType::Control,
+            smoothness: SmoothnessType::Break,
         }
     }
 }
@@ -27,4 +33,13 @@ impl BezierPoint {
 pub enum BezierPointType {
     Control,
     Defining,
+}
+
+#[derive(Debug, Copy, Clone)]
+pub enum SmoothnessType {
+    // If point is defining
+    None,
+
+    Break,
+    Smooth,
 }
