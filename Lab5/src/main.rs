aaffa64224588eb9f280291ee537f0d517772131
xairaven <alexkovalevkpi@gmail.com>
2024-10-31 23:27:57 +0200

    Lab 5, Feat: Logger, Config, Custom Errors

--- Lab5/src/main.rs
+++ Lab5/src/main.rs
@@ -1,3 +1,27 @@
+// Hide console window on Windows in release mode
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use crate::config::AppConfig;
+
 fn main() {
-    println!("Hello, world!");
+    let app_config = AppConfig::from_env().unwrap_or_else(|err| {
+        println!("Error: {err}");
+        std::process::exit(1);
+    });
+
+    logger::init(
+        app_config.log_level,
+        &logger::generate_log_name(&app_config.name),
+    )
+    .unwrap_or_else(|err| {
+        println!("Logger initialization failed. Error: {}", err);
+        std::process::exit(1);
+    });
 }
+
+pub mod config;
+pub mod errors {
+    pub mod env;
+    pub mod log;
+}
+pub mod logger;
